932b87849b32fad117f1baf6e40853a9dd1d6474
viniciobs <vinicio.barreto.santos@gmail.com>
2021-06-18 20:43:15 -0300

    Creating PermissionException

---
+++ Domain/Exceptions/PermissionException.cs
@@ -0,0 +1,19 @@
+﻿using System.Security;
+
+namespace Domains.Exceptions
+{
+	public class PermissionException : SecurityException
+	{
+		#region Constructors
+
+		public PermissionException()
+			: base()
+		{ }
+
+		public PermissionException(string message)
+			: base(message)
+		{ }
+
+		#endregion Constructors
+	}
+}
