ea2984c11c04923de87b87a08606c85cc66d6dd5
viniciobs <vinicio.barreto.santos@gmail.com>
2022-02-20 23:50:15 -0300

    Adding tests to token generator service

--- ToDoList/Services/TokenGenerator/Models/ClaimsData.cs
+++ ToDoList/Services/TokenGenerator/Models/ClaimsData.cs
@@ -9,8 +9,13 @@ namespace ToDoList.API.Services.TokenGenerator.Models
         public Guid UserId { get; private set; }
         public UserRole UserRole { get; private set; }
 
+        private ClaimsData()
+        { }
+
         public static ClaimsData Convert(AuthenticationResult authenticationResult)
         {
+            if (authenticationResult == null) throw new ArgumentNullException(nameof(authenticationResult));
+
             return new ClaimsData()
             {
                 UserId = authenticationResult.UserId,
---
+++ TodoList.Test/Services/TokenGenerator.cs
@@ -0,0 +1,50 @@
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using Repository.DTOs.Accounts;
+using System;
+using ToDoList.API.Services.TokenGenerator;
+using ToDoList.API.Services.TokenGenerator.Interfaces;
+using ToDoList.API.Services.TokenGenerator.Models;
+using ToDoList.UI.Configurations;
+
+namespace TodoList.Test.Services
+{
+    public class Tests
+    {
+        private ITokenGenerator tokenGenerator;
+
+        [SetUp]
+        public void Setup()
+        {
+            Authentication authentication = new Authentication() { Secret = Guid.NewGuid().ToString() };
+            IOptions<Authentication> authOptions = Options.Create(authentication);
+
+            tokenGenerator = new TokenGenerator(authOptions);
+        }
+
+        [Test]
+        public void TestTokenGenerator_CreateClaimsDataWithNullEntry_ThrowsNullArgumentException()
+        {
+            Assert.Throws<ArgumentNullException>(() => ClaimsData.Convert(null));
+        }
+
+        [Test]
+        public void TestTokenGeneratorWithNullEntry_ThrowsNullArgumentException()
+        {
+            Assert.Throws<ArgumentNullException>(() => tokenGenerator.GenerateToken(null));
+        }
+
+        [Test]
+        public void TestTokenGenerator_Ok()
+        {
+            AuthenticationResult authenticationResult = new AuthenticationResult()
+            {
+                UserId = Guid.NewGuid(),
+                Role = Domains.UserRole.Admin
+            };
+
+            string token = tokenGenerator.GenerateToken(ClaimsData.Convert(authenticationResult));
+            Assert.IsNotNull(token);
+        }
+    }
+}
