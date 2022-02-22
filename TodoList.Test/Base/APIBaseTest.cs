95ca4c70537d226ae7edd931950bafa3458789f3
viniciobs <vinicio.barreto.santos@gmail.com>
2022-02-21 22:32:45 -0300

    Test bad request for account creation

---
+++ TodoList.Test/Base/APIBaseTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using ToDoList.UI;
+
+namespace TodoList.API.Test.Base
+{
+    public class APIBaseTest
+    {
+        protected readonly HttpClient _client;
+
+        public APIBaseTest()
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(currentDirectory)
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets("b8bf55d2-cabd-4731-9649-5e3ac0b548ca")
+                .Build();
+
+            var webHostBuilder = new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseConfiguration(configuration)
+                .UseStartup<Startup>();
+
+            var server = new TestServer(webHostBuilder);
+
+            _client = server.CreateClient();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
--- TodoList.Test/Controllers/AccountsControllerTests.cs
+++ TodoList.Test/Controllers/AccountsControllerTests.cs
@@ -1,62 +1,48 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.Configuration;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Repository.DTOs.Accounts;
-using System.IO;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text.Json;
 using System.Threading.Tasks;
+using TodoList.API.Test.Base;
 using TodoList.API.Test.Helpers;
-using ToDoList.UI;
 
 namespace TodoList.API.Test.Controllers
 {
-    public class AccountsControllerTests
+    public class AccountsControllerTests : APIBaseTest
     {
-        private readonly HttpClient _client;
-
-        public AccountsControllerTests()
-        {
-            var currentDirectory = Directory.GetCurrentDirectory();
-
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(currentDirectory)
-                .AddJsonFile("appsettings.json")
-                .AddUserSecrets("b8bf55d2-cabd-4731-9649-5e3ac0b548ca")
-                .Build();
-
-            var webHostBuilder = new WebHostBuilder()
-                .UseEnvironment("Development")
-                .UseConfiguration(configuration)
-                .UseStartup<Startup>();
-
-            var server = new TestServer(webHostBuilder);
-
-            _client = server.CreateClient();
-            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        }
-
         [Theory]
         [TestCase("", " ")]
         [TestCase("", null)]
-        [TestCase(null, null)]
-        [TestCase(null, "")]
-        [TestCase("foo", "")]
-        [TestCase("foo", null)]
         [TestCase(null, "bar")]
         [TestCase(" ", "bar")]
         public async Task TryAuthenticateWithEmptyData_ReturnBadRequest(string login, string password)
         {
-            //Arange
+            // Arange
             var emptyData = new AuthenticationData();
             emptyData.Login = login;
             emptyData.Password = password;
 
             // Act
             var response = await _client.PostAsync("accounts/authenticate", emptyData.AsStringContent());
+
+            // Assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.BadRequest);
+        }
+
+        [Theory]
+        [TestCase("", "", "")]
+        [TestCase("", "   ", null)]
+        [TestCase("foo", "bar", "")]
+        [TestCase("foo", null, "bar")]
+        public async Task TryCreateNewAccountWithEmptyData_ReturnBadRequest(string name, string login, string password)
+        {
+            // Arrange
+            var emptyData = new CreateAccountData();
+            emptyData.Name = name;
+            emptyData.Login = login;
+            emptyData.Password = password;
+
+            // Act
+            var response = await _client.PostAsync("accounts/new", emptyData.AsStringContent());
 
             // Assert
             Assert.AreEqual(response.StatusCode, HttpStatusCode.BadRequest);
