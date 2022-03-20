de4b205dc87cc01d175d175e8ae879cfb6dfa59c
viniciobs <vinicio.barreto.santos@gmail.com>
2022-03-20 20:53:45 -0300

    Apply clean code to task comments test

--- Domain.Tests/TaskCommentsTest.cs
+++ Domain.Tests/TaskCommentsTest.cs
@@ -1,76 +1,35 @@
 ﻿using Domains.Exceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Linq;
 
 namespace Domains.Tests
 {
-	[TestClass]
-	public class TaskCommentsTest : DomainTestBase
-	{
-		[TestMethod]
-		public void TestAddTaskThrowMissingArgumentsException()
-		{
-			// Arrange
-			var user = GenerateRandomUser();
-			var task = user.AssignTask(GenerateRandomUser(), ValidTaskDescription);
+    [TestClass]
+    public class TaskCommentsTest : DomainTestBase
+    {
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow("  ")]
+        public void AddNullComment_ThrowMissingArgumentsException(string comment)
+        {
+            var task = normalUser.SelfAssignTask("Test");
 
-			// Act and assert
-			Assert.ThrowsException<MissingArgumentsException>(() => user.AddComment(NullTask, ValidComment));
-			Assert.ThrowsException<MissingArgumentsException>(() => user.AddComment(task, NullComment));
-			Assert.ThrowsException<MissingArgumentsException>(() => user.AddComment(task, EmptyComment));
-		}
+            Assert.ThrowsException<MissingArgumentsException>(() => normalUser.AddComment(task, comment));
+        }
 
-		[TestMethod]
-		public void TestAddCommentThrowPermissionException()
-		{
-			// Act and assert
-			Assert.ThrowsException<PermissionException>(() => GenerateRandomUser().AddComment(GenerateRandomTask(), ValidComment));
-		}
+        [TestMethod]
+        public void RandomUserTryAddComment_ThrowPermissionException()
+        {
+            var task = normalUser.SelfAssignTask("Test");
+            var randomUser = GenerateRandomUser();
 
-		[TestMethod]
-		public void TestAddCommentThrowMissingArgumentsException()
-		{
-			// Arrange
-			var user = GenerateRandomUser();
-			var task = user.AssignTask(GenerateRandomUser(), ValidTaskDescription);
+            Assert.ThrowsException<PermissionException>(() => randomUser.AddComment(task, "Test"));
+        }
 
-			// Act and assert
-			Assert.ThrowsException<MissingArgumentsException>(() => user.AddComment(task, NullComment));
-			Assert.ThrowsException<MissingArgumentsException>(() => user.AddComment(task, EmptyComment));
-		}
-
-		[TestMethod]
-		public void TestAddCommentOk()
-		{
-			// Arrange
-			var user = GenerateRandomUser();
-			var task = user.AssignTask(GenerateRandomUser(), ValidTaskDescription);
-
-			user.AddComment(task, ValidComment);
-
-			// Act and assert
-			Assert.AreEqual(ValidComment, task.Comments.Single().Text);
-		}
-
-		[TestMethod]
-		public void TestAddMultipleCommentOk()
-		{
-			// Arrange
-			var user = GenerateRandomUser();
-			var task = user.AssignTask(user, ValidTaskDescription);
-			var comment1 = GenerateRandomString();
-			var comment2 = GenerateRandomString();
-			var comment3 = GenerateRandomString();
-
-			user.AddComment(task, comment1);
-			user.AddComment(task, comment2);
-			user.AddComment(task, comment3);
-
-			// Act and assert
-			Assert.AreEqual(comment1, task.Comments.Where((x, i) => i == 0).Single().Text);
-			Assert.AreEqual(comment2, task.Comments.Where((x, i) => i == 1).Single().Text);
-			Assert.AreEqual(comment3, task.Comments.Where((x, i) => i == 2).Single().Text);
-			Assert.AreEqual(3, task.Comments.Count);
-		}
-	}
+        [TestMethod]
+        public void TryAddCommentToNullTask_ThrowMissingArgumentsException()
+        {
+            Assert.ThrowsException<MissingArgumentsException>(() => normalUser.AddComment(null, "Test"));
+        }
+    }
 }
--- Domain/TaskComment.cs
+++ Domain/TaskComment.cs
@@ -4,51 +4,53 @@ using Task = Domains.User.Task;
 
 namespace Domains
 {
-	public partial class User
-	{
-		public partial class Task
-		{
-			public class TaskComment
-			{
-				public Guid Id { get; private set; }
-				public string Text { get; private set; }
-				public Guid TaskId { get; private set; }
-				public Task Task { get; private set; }
-				public Guid CreatedByUserId { get; private set; }
-				public User CreatedBy { get; private set; }
-				public DateTime CreatedAt { get; private set; }
+    public partial class User
+    {
+        public partial class Task
+        {
+            public class TaskComment
+            {
+                public Guid Id { get; private set; }
+                public string Text { get; private set; }
+                public Guid TaskId { get; private set; }
+                public Task Task { get; private set; }
+                public Guid CreatedByUserId { get; private set; }
+                public User CreatedBy { get; private set; }
+                public DateTime CreatedAt { get; private set; }
 
-				public TaskComment()
-				{ }
+                public TaskComment()
+                { }
 
-				private TaskComment(Task task, User creator, string text)
-				{
-					if (task == null) throw new MissingArgumentsException(nameof(task));
-					if (creator == null) throw new MissingArgumentsException(nameof(creator));
-					if (string.IsNullOrEmpty(text)) throw new MissingArgumentsException(nameof(text));
+                private TaskComment(Task task, User creator, string text)
+                {
+                    if (task == null) throw new MissingArgumentsException(nameof(task));
+                    if (creator == null) throw new MissingArgumentsException(nameof(creator));
+                    if (string.IsNullOrEmpty(text?.Trim())) throw new MissingArgumentsException(nameof(text));
 
-					CreatedBy = creator;
-					CreatedByUserId = creator.Id;
-					Task = task;
-					TaskId = task.Id;
-					Text = text;
-					CreatedAt = DateTime.UtcNow;
-				}
+                    CreatedBy = creator;
+                    CreatedByUserId = creator.Id;
+                    Task = task;
+                    TaskId = task.Id;
+                    Text = text;
+                    CreatedAt = DateTime.UtcNow;
+                }
 
-				internal static TaskComment New(Task task, User creator, string text)
-				{
-					return new TaskComment(task, creator, text);
-				}
+                internal static TaskComment New(Task task, User creator, string text)
+                {
+                    return new TaskComment(task, creator, text);
+                }
 
 #if DEBUG
-				// Alter the CreatedAt property.
-				// Must be used only for tests purposes.				
-				public void AlterCreatedAt(DateTime date)
-				{
-					CreatedAt = date;
-				}
+
+                // Alter the CreatedAt property.
+                // Must be used only for tests purposes.
+                public void AlterCreatedAt(DateTime date)
+                {
+                    CreatedAt = date;
+                }
+
 #endif
-			}
-		}
-	}
+            }
+        }
+    }
 }
