0c3617a17828c2a39e265c0d73da17fe8351fa7c
viniciobs <vinicio.barreto.santos@gmail.com>
2022-03-31 22:27:10 -0300

    Implementing Period tests

--- Repository/DTOs/_Commom/Period.cs
+++ Repository/DTOs/_Commom/Period.cs
@@ -3,28 +3,28 @@ using System;
 
 namespace Repository.DTOs._Commom
 {
-	public class Period
-	{		
-		public DateTime? Start { get; private set; }
-		public DateTime? End { get; private set; }
-		public bool HasValue => Start.HasValue || End.HasValue;
+    public class Period
+    {
+        public DateTime? Start { get; private set; }
+        public DateTime? End { get; private set; }
+        public bool HasValue => Start.HasValue || End.HasValue;
 
-		public Period(DateTime? start, DateTime? end)
-		{
-			if (start > end) throw new RuleException("Start date can't be higher than end date");
+        public Period(DateTime? start, DateTime? end)
+        {
+            if (start > end) throw new RuleException("Start date can't be higher than end date");
 
-			Start = start;
-			End = end;
-		}
+            Start = start;
+            End = end;
+        }
 
-		public bool IsBetween(DateTime date)
-		{
-			if (Start == null)
-				return End > date;
-			else if (End == null)
-				return date > Start;
-			else
-				return date > Start && date < End;
-		}
-	}
+        public bool IsBetween(DateTime date)
+        {
+            if (Start == null)
+                return End >= date;
+            else if (End == null)
+                return date >= Start;
+            else
+                return date >= Start && date <= End;
+        }
+    }
 }
