d416bac5ba9092ba76b008540c7ec5d5875cfa26
viniciobs <vinicio.barreto.santos@gmail.com>
2022-05-18 22:13:31 -0300

    Remove environment check for test in API

--- ToDoList/Services/MessageBroker/Sender/Azure/HistorySender.cs
+++ ToDoList/Services/MessageBroker/Sender/Azure/HistorySender.cs
@@ -13,13 +13,8 @@ namespace ToDoList.API.Services.MessageBroker.Sender
         private readonly ServiceBusClient _client;
         private readonly ServiceBusSender _sender;
 
-        private readonly bool _isTestEnvironment;
-
-        public HistorySender(IConfiguration configuration, IWebHostEnvironment env)
+        public HistorySender(IConfiguration configuration)
         {
-            _isTestEnvironment = env.EnvironmentName == "Test";
-            if (_isTestEnvironment) return;
-
             string connectionString = configuration.GetBrokerConnection();
             if (string.IsNullOrEmpty(connectionString?.Trim())) throw new ArgumentNullException("Configure your connection string for Microsoft Azure ServiceBus");
 
@@ -29,8 +24,6 @@ namespace ToDoList.API.Services.MessageBroker.Sender
 
         public async Task PostHistoryAsync(HistoryData history)
         {
-            if (_isTestEnvironment) return;
-
             var payload = JsonSerializer.Serialize(history);
             var message = new ServiceBusMessage(payload);
 
